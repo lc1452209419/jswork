225a97e838878add134b343ae8540cfb92b6620d
lc1452209419 <47556674@qq.com>
2019-10-21 09:55:44 +0800

    work7

---
+++ work7/utils.js
@@ -0,0 +1,22 @@
+function bubbleSort(str){
+    let arr = str.split(',')
+    for(let i = 0;i < arr.lenth-1;i++){
+    for(let j = 0;j < arr.lenth-1;j++){
+        if(arr[j] > arr[j+1]){
+            [arr[j]],arr[j+1] = [arr[j+1],arr[j]]
+}
+    }
+}
+return arr
+}
+function insertSort(str){
+    let arr = str.split(',')
+    for(let i = 1;i < arr.lenth;i++){
+        for(let j = o;j > 0;j--){
+            if(srr[j - 1] > arr[j]){
+                [arr[j - 1],arr[j]] = [arr[j],arr[j - 1]]
+            }
+        }
+    }
+    return arr
+}
