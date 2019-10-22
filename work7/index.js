e9b8cd1111ea354a66318a9af00ed5670513f125
lc1452209419 <47556674@qq.com>
2019-10-22 12:36:44 +0800

    work7

--- work7/index.js
+++ work7/index.js
@@ -2,11 +2,11 @@ function sort(method){
     let origin = document.getElementById('origin').value
     let logs
     console.log(method)
-    if(method == 'bubble'){
-        logs = bubbleSort(origin)
-    }else if(method == 'insert'){
-        logs = insertSort(origin)
-    }  
-    showDiv = document.getElementById('Amin')
-    showDiv.innerHtml = logs
+    if(method=='bubble'){
+        logs=bubbleSort(origin)
+    }else if(method=='insert'){
+        logs=insertSort(origin)
+    }
+    showDiv=document.getElementById('Anim')
+    showDiv.innerHTML=logs
 }
--- work7/utils.js
+++ work7/utils.js
@@ -1,9 +1,10 @@
+//
 function bubbleSort(str){
     let arr = str.split(',')
     for(let i = 0;i < arr.lenth-1;i++){
-    for(let j = 0;j < arr.lenth-1;j++){
-        if(arr[j] > arr[j+1]){
-            [arr[j]],arr[j+1] = [arr[j+1],arr[j]]
+    for(let j = 0;j < arr.lenth-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
 }
     }
 }
@@ -12,7 +13,7 @@ return arr
 function insertSort(str){
     let arr = str.split(',')
     for(let i = 1;i < arr.lenth;i++){
-        for(let j = o;j > 0;j--){
+        for(let j = i;j > 0;j--){
             if(srr[j - 1] > arr[j]){
                 [arr[j - 1],arr[j]] = [arr[j],arr[j - 1]]
             }
